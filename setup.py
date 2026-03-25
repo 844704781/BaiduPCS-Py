1188b3f005be1178daf8f9c0352d36d7094ab323
watermelon <844704781@qq.com>
2026-03-25 14:35:25 +0800

    fix: 添加 packages 配置使 poetry 正确发现 baidupcs_py

--- setup.py
+++ setup.py
@@ -1,28 +1,8 @@
 #!/usr/bin/env python
 
-import os
-from setuptools import setup, find_packages
-from Cython.Build import cythonize
-from distutils.command.build_ext import build_ext
+# This is a shim to hopefully allow Github to detect the package, build is done with poetry
 
-# 定义 Cython 扩展
-extensions = ["baidupcs_py/common/simple_cipher.pyx"]
+import setuptools
 
-# gcc arguments hack: enable optimizations
-os.environ["CFLAGS"] = "-O3"
-
-ext_modules = cythonize(
-    extensions,
-    language_level=3,
-    compiler_directives={"linetrace": True},
-)
-
-if ext_modules:
-    ext_modules[0].name = "baidupcs_py.common.simple_cipher"
-
-setup(
-    name="baidupcs-py",
-    packages=find_packages(exclude=["imgs", "tests", "tests.*"]),
-    ext_modules=ext_modules,
-    cmdclass={"build_ext": build_ext},
-)
+if __name__ == "__main__":
+    setuptools.setup(name="baidupcs-py")
