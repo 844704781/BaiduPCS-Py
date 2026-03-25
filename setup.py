07d843086dcd63393d81454f38ce37be6f9da671
watermelon <844704781@qq.com>
2026-03-25 14:28:47 +0800

    fix: setup.py 支持 Cython 编译 simple_cipher

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python
 
-# This is a shim to hopefully allow Github to detect the package, build is done with poetry
+import os
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+from distutils.command.build_ext import build_ext
 
-import setuptools
+# 定义 Cython 扩展
+extensions = ["baidupcs_py/common/simple_cipher.pyx"]
 
-if __name__ == "__main__":
-    setuptools.setup(
-        name="baidupcs-py",
-        packages=setuptools.find_packages(exclude=["imgs", "tests", "tests.*"]),
-    )
+# gcc arguments hack: enable optimizations
+os.environ["CFLAGS"] = "-O3"
+
+ext_modules = cythonize(
+    extensions,
+    language_level=3,
+    compiler_directives={"linetrace": True},
+)
+
+if ext_modules:
+    ext_modules[0].name = "baidupcs_py.common.simple_cipher"
+
+setup(
+    name="baidupcs-py",
+    packages=find_packages(exclude=["imgs", "tests", "tests.*"]),
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+)
